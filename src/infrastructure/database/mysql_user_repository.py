f3f940e21b6dba9383549618f32372c5a7ae53bf
Eltondrknss <eltonoliveira1988@gmail.com>
2025-08-25 22:32:49 -0300

    Criados metodos faltantes para cumprir o contrato do User Repository

--- src/infrastructure/database/mysql_user_repository.py
+++ src/infrastructure/database/mysql_user_repository.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from src.core.entities.user import UserRole
+from src.core.entities.user import User
 from src.core.entities.user_role import UserRole
 from src.core.repositories.user_repository import IUserRepository
 from .db_connection_handler import db_handler
@@ -14,13 +14,24 @@ class MySQLUserRepository(IUserRepository):
             INSERT INTO {self.table_name} (name, email, password_hash, role)
             VALUES (%s, %s, %s, %s)
         """
-
         params = (user.name, user.email, user.password_hash, user.role_value)
 
         with db_handler.managed_cursor() as cursor:
             cursor.execute(query, params)
             user.id = cursor.lastrowid
         return user
+    
+    def find_by_id(self, user_id: int) -> Optional[User]:
+        query = f"SELECT id, name, email, password_hash, role FROM {self.table_name} WHERE id = %s"
+
+        with db_handler.managed_cursor() as cursor:
+            cursor.execute(query, (user_id,))
+            row = cursor.fetchone()
+
+        if row:
+            row['role'] = UserRole(row['role'])
+            return User(**row)
+        return None
     
     def find_by_email(self, email: str) -> Optional[User]:
         query = f"SELECT id, name, email, password_hash, role FROM {self.table_name} WHERE email = %s"
@@ -34,3 +45,17 @@ class MySQLUserRepository(IUserRepository):
             return User(**row)
         return None
     
+    def find_all(self) -> List[User]:
+        query = f"SELECT id, name, email, password_hash, role FROM {self.table_name}"
+
+        users: List[User] = []
+
+        with db_handler.managed_cursor() as cursor:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+
+        for row in rows:
+            row['role'] = UserRole(row['role'])
+            users.append(User(**row))
+
+        return users
