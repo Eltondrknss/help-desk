3769b9dd6a02e77525e1ef203c966ba2140d6b1d
Eltondrknss <eltonoliveira1988@gmail.com>
2025-09-03 23:28:31 -0300

    Refactor Ticket and User classes to use Pydantic models for validation and data handling

--- src/core/entities/ticket.py
+++ src/core/entities/ticket.py
@@ -1,18 +1,20 @@
-from dataclasses import dataclass, field
+from pydantic import BaseModel, Field, constr
 from datetime import datetime
 from typing import Optional
 from .ticket_status import TicketStatus
 
-@dataclass
-class Ticket:
-
-    title: str
-    description: str
+class Ticket(BaseModel):
+    
+    title: constr(strip_whitespace=True, min_length=5)
+    description: constr(strip_whitespace=True, min_length=1)
     status: TicketStatus
     user_id: int
 
     id: Optional[int] = None
     technician_id: Optional[int] = None
 
-    created_at: datetime = field(default_factory=datetime.now)
-    updated_at: datetime = field(default_factory=datetime.now)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+class Config:
+    from_attributes = True
--- src/core/entities/user.py
+++ src/core/entities/user.py
@@ -1,11 +1,14 @@
-from dataclasses import dataclass
+from pydantic import BaseModel, EmailStr, constr
 from typing import Optional
 from .user_role import UserRole
 
-@dataclass
-class User:
-    name: str
-    email: str
+class User(BaseModel):
+
+    name: constr(strip_whitespace=True, min_length=4)
+    email: EmailStr
     password_hash: str
     role: UserRole
     id: Optional[int] = None
+
+class Config:
+    from_attributes = True
